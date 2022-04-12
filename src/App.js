e5aa0fc6ce27aa9b099aff79452901f16cd4cd1f
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-12 22:17:07 +0200

    ft(Header): added header component to Root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Layout/>
     </div>
   );
