e33b94542e162a9cc28715777713f4328ac6a02f
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-12 22:20:05 +0200

    ft(Balance): created balance component and added it to the root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Balance from './components/Balance';
 import Header from './components/Header';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div className="container">
+        <Balance/>
+       </div>
       <Layout/>
     </div>
   );
---
+++ src/components/Balance.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+
+function Balance() {
+	return (
+		<div>
+			<h4>Your Balance</h4>
+			<h1>300 Rwf</h1>
+		</div>
+	);
+}
+
+export default Balance;
