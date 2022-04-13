efe7381fbb4760e9a553cd38d6d6c313016f65e0
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-13 22:51:59 +0200

    ft(Expense): created expense form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import Balance from './components/Balance';
-import Header from './components/Header';
-import IncomeExpense from './components/IncomeExpense';
-import Layout from './components/Layout';
+/** @format */
+
+import "./App.css";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpense from "./components/IncomeExpense";
+import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpense/>
-       </div>
-      <Layout/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExpense />
+			</div>
+			<Layout />
+		</div>
+	);
 }
 
 export default App;
